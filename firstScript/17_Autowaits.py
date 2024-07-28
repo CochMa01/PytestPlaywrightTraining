cf2fb8ae8aa18d7061cf7872e6726935d0b2195c
CochMa01 <CochMa01@risk.regn.net>
2024-07-28 20:08:14 +0800

    Updates pt. 5

--- firstScript/17_Autowaits.py
+++ firstScript/17_Autowaits.py
@@ -3,8 +3,13 @@ from playwright.sync_api import sync_playwright
 with (sync_playwright() as playwright):
     browser = playwright.chromium.launch(headless=False, slow_mo=3000)
     context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://demo.nopcommerce.com/")
 
-
+    register_link_elem = page.locator("//a[@class='ico-register']")
+    register_link_elem.highlight()
+    register_link_elem.click()
+    page.wait_for_timeout(1000)
 
     # ----------------------------------------
     context.close()
@@ -13,4 +18,3 @@ with (sync_playwright() as playwright):
     # Copy the code from 14_SelectDropdownActions.py.py and rename it to 15_FileUploadActions.py.
     # On this script, we will focus on how we can automate Radio buttons and Checkbox actions.
     # Update first the URL to https://davidwalsh.name/demo/multiple-file-upload.php.
-
